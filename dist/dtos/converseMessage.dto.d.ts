468ca2bbf704761ee92fee1d9bf807ebb0db38ee
Ayomide Temitayo Bamigboye <42998943+Aycom366@users.noreply.github.com>
2023-07-31 09:35:49 +0100

    Delete dist directory

--- dist/dtos/converseMessage.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { MessageType } from "@prisma/client";
-export declare class CreateSingleConversation {
-    userId: number;
-}
-export declare class SendMessageDto {
-    messageType?: MessageType;
-    message: string;
-    conversationId: number;
-}
--- dist/user/user.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserService } from "./user.service";
-import { PrismaService } from "src/prisma/prisma.service";
-export declare class UserController {
-    private readonly userService;
-    private readonly prismaService;
-    constructor(userService: UserService, prismaService: PrismaService);
-    getAllUser(request: any): Promise<{}>;
-}
